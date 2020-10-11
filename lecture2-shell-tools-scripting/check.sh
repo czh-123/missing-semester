cfc6b980aa5677067b7ce9249182a54261919649
cczzhh <1261340248@qq.com>
2020-10-11 01:10:18 -0700

    finish 3 completely

--- lecture2-shell-tools-scripting/check.sh
+++ lecture2-shell-tools-scripting/check.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+error="0"
 count="0"
 file="testcase3.sh"
-bash ./$file 1>>run.log 2>>error.log  #use bash always because of [[ ]]
+#bash ./$file 1>run.log 2>error.log  #use bash always because of [[ ]]
 
-while [[ $? -eq 0 ]];do        #always use whitespace
+while [[ $error -eq 0 ]];do        #always use whitespace
 	bash ./$file 1>>run.log 2>>error.log
+	if [[ $? -ne 0 ]];then
+		error="1"
+	fi
+	count=$[$count+1]   # $[] means suanshu alg
 done
 
-echo "run $0 finished"
+echo "run $0 finished run $count times"
 
